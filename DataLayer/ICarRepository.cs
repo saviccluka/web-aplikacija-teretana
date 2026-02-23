2aec93a386b6026374a4db28b141f82026ee8fe1
saviccluka <lukasavic6969@gmail.com>
2026-02-23 21:35:46 +0100

    Add files via upload

---
+++ DataLayer/ICarRepository.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Model;
+
+namespace DataLayer
+{
+    public interface ICarRepository
+    {
+        // CREATE
+        bool Create(Car car);
+
+        // READ
+        List<Car> GetCars();
+        Car? GetById(int id);
+
+        // UPDATE
+        bool Update(Car car);
+
+        // DELETE
+        bool Delete(Car car);
+    }
+}
