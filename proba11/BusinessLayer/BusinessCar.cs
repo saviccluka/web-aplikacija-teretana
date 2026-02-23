1642a8f838bcba8aa26f7dcb1f215789faf34ced
saviccluka <lukasavic6969@gmail.com>
2026-02-23 19:17:13 +0100

    Add files via upload

---
+++ proba11/BusinessLayer/BusinessCar.cs
@@ -0,0 +1,36 @@
+﻿using DataLayer;
+using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BusinessCar : IBusinessCar
+    {
+        private readonly ICarRepository carRepository;
+
+        public BusinessCar(ICarRepository carRepository)
+        {
+            this.carRepository = carRepository;
+        }
+
+        public string Delete(Car car)
+        {
+            if (carRepository.Delete(car))
+            {
+                return "Automobil obrisan";
+           }
+            return "Greska";
+        }
+
+        public List<Car> GetCars()
+        {
+            var x = from item in carRepository.GetCars()
+                    orderby item.Price descending select item;
+            return x.ToList();
+        }
+    }
+}
---
+++ proba11/BusinessLayer/IBusinessCar.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface IBusinessCar
+    {
+        List<Car> GetCars();
+        string Delete(Car car);
+    }
+}
